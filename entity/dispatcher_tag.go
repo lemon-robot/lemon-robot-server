23eedb8be31c5f9eafa7f6690f7a6186c9203c90
LemonITCN <liuri@lemonit.cn>
2019-05-14 16:36:55 +0800

    设置update at 字段为不显示在json中

--- entity/dispatcher_tag.go
+++ entity/dispatcher_tag.go
@@ -6,5 +6,5 @@ type DispatcherTag struct {
 	TagKey    string    `gorm:"primary_key;size:64" json:"tagKey"`
 	TagName   string    `gorm:"size:32" json:"tagName"`
 	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	UpdatedAt time.Time `json:"-"`
 }
