d68efd920bd290e1be9241d52a7e5e2163bf2ff1
LemonITCN <liuri@lemonit.cn>
2019-07-26 15:08:37 +0800

    修改file_Resource为其他机器存储

--- entity/file_resource.go
+++ entity/file_resource.go
@@ -4,13 +4,13 @@ import "time"
 
 type FileResource struct {
 	FileResourceKey  string `gorm:"primary_key;size:64" json:"fileResourceKey"`
-	UsedTag          bool   `json:"usedTag"`
-	UploadedTag      bool   `json:"uploadedTag"`
+	SourceType       string `gorm:"size:128" json:"sourceType"`
 	OriginalFileName string `gorm:"size:512" json:"originalFileName"`
 	FileExtension    string `gorm:"size:512" json:"fileExtension"`
 	FileSize         int64  `json:"fileSize"`
-	FileContent      []byte `gorm:"size:2048000" json:"fileContent"`
+	FilePath         string `gorm:"size:4096" json:"filePath"`
 	ContentType      string `gorm:"size:128" json:"contentType"`
 	CreatedAt        time.Time
 	UpdatedAt        time.Time
+	DeleteAt         time.Time
 }
